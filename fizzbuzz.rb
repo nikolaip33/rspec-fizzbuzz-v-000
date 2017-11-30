0a071a3a1cefb26b3e28b3bc41c5175875983494
Nikolai Pawlowski <nikolaip@gmail.com>
2017-11-30 14:33:44 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,12 @@
+<<<<<<< HEAD
 def fizzbuzz(n)
   n%15 == 0 ? 'FizzBuzz' : n%3 == 0 ? 'Fizz' : n%5 == 0 ? 'Buzz' : nil
 end
+=======
+# def fizzbuzz(n)
+#   n%15 == 0 ? 'FizzBuzz' : n%3 == 0 ? 'Fizz' : n%5 == 0 ? 'Buzz' : nil
+# end
+>>>>>>> fd55dff8244fc9d35781a7965af2d12712555be3
 
 
 # def fizzbuzz(num)
